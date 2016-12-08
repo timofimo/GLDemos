9e87283e395be49648c4013ff8b585c0980a07c9
Unknown <timo.van.hees@hotmail.com>
2016-12-08 22:48:08 +0100

    (BROKEN) Commit to copy work from one PC to another

--- GL/include/GL.h
+++ GL/include/GL.h
@@ -1,13 +1,25 @@
 #pragma once
 #include "Window.h"
+#include <vector>
 
 namespace GL
 {
-
-	class Renderer
+	enum class EAttributeLayout
 	{
-	public:
-		void Initialize(const IWindow* window);
+		eFloat, eVec2, eVec3, eVec4,
+		eInt, eIVec2, eIVec3, eIVec4,
+		eUInt, eUVec2, eUVec3, eUVec4,
+		eDouble, eDVec2, eDVec3, eDVec4,
+		eMat3x3, eMat4x4,
 	};
+
+	Mesh CreateMesh(const float* vertexData, const std::vector<VertexConfiguration>& vertexConfiguration, const std::vector<unsigned>& indexData = std::vector<unsigned>());
+	Shader CreateShader(const char* vertexShader, const char* fragmentShader);
+
+	void Clear(float r, float g, float b, float a);
+	void DrawIndexed(unsigned count);
+
+	void BindMesh(const Mesh& mesh);
+	void BindShader(const Shader& shader);
 
 }
--- TriangleDemo/TriangleDemo.cpp
+++ TriangleDemo/TriangleDemo.cpp
@@ -1,12 +1,31 @@
 #include "GL.h"
 #include "Window.h"
+#include "glm/glm.hpp"
 
 int main()
 {
-	GL::GLFWWindow window(640, 480, "TriangleDemo", true);
+	GL::GLFWWindow window(640, 480, "TriangleDemo", false);
+
+	const float vertices[] = 
+		{ 
+			-1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 
+			0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+			1.0f, -1.0f, 0.0f, 0.0f, 1.0f
+		};
+	const unsigned indices[] = { 0, 1, 2 };
+	Mesh triangle = GL::CreateMesh(vertices, 15, indices, 3, { GL::EAttributeLayout::eVec2/*Pos*/, GL::EAttributeLayout::eVec3/*Col*/ }, 2);
+
+	Shader shader = GL::CreateShader("res/shaders/triangleDemo.vert", "res/shaders/triangleDemo.frag");
+
+	GL::BindMesh(triangle);
+	GL::BindShader(shader);
 
 	while(!window.ShouldClose())
 	{
+		GL::Clear(0.0f, 0.0f, 0.0f, 1.0f);
+
+		GL::DrawIndexed(3);
+
 		window.SwapBuffers();
 	}
 
